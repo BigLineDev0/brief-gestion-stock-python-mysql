b8fe9ca22c4c4ece1328592c7eacdb62270440f4
mamesayelom <mamesayelom04@gmail.com>
2026-02-11 15:04:41 +0000

    crypter le mot de pass de l'utilisateur et ajouter les roles pour chaque utilisateur

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from gestion_stock import gestion_categorie, gestion_produit, effectuer_mouvement, afficher_historiques
-from model.utilisateur import Ajouter_utilisateur,afficher_utilisateur
+from model.utilisateur import Ajouter_utilisateur,utilisateur_connecter,mot_de_pass_hash,afficher_utilisateur
 from model.produits import alerte_produits
+from model.categories import afficher_categorie
+from model.produits import afficher_produits
+import bcrypt
 # Fonction afficher le menu
-def menu():
+def menu_admin():
 
     print("\n----------- GESTION STOCK  BOUTIQUE PRO -------------")
     print("1. Gestion des catégories")
@@ -15,10 +18,20 @@ def menu():
     print("0. Quitter")
     print("-" * 40)
 
+def menu_user():
+    print("\n----------- GESTION STOCK  BOUTIQUE PRO -------------")
+    print("1. Afficher categorie")
+    print("2. Afficher produits")
+    print("3. Alerte stock faible (< 5)")
+    print("4. Historique des mouvements")
+    print("5. Afficher les utilisateurs")
+    print("0. Quitter")
+    print("-" * 40)
+
 # Démarage du programme
-def main():
+def Admin():
     while  True:
-        menu()
+        menu_admin()
         choix = input("\nChoisissez une option : ")
         match choix:
             case '1':
@@ -39,6 +52,35 @@ def main():
             case '6':
                 Ajouter_utilisateur()
 
+            case '7':
+                afficher_utilisateur()
+
+            case '0':
+                print("Au revoir!")
+                break 
+            case _:
+                print("\nOption invalide, veuillez réessayer.")
+
+def User():
+    while  True:
+        menu_user()
+        choix = input("\nChoisissez une option : ")
+        match choix:
+            case '1':
+                afficher_categorie()
+
+            case '2':
+                afficher_produits()
+            
+            case '3':
+                alerte_produits()
+            
+            case '4':
+                afficher_historiques()
+            
+            case '5':
+                afficher_utilisateur()
+
             case '0':
                 print("Au revoir!")
                 break 
@@ -46,16 +88,39 @@ def main():
                 print("\nOption invalide, veuillez réessayer.")
 
 
-def auth():
+def main():
     print('----------------Connexion----------------------')
     print()
     while True:
-        email=input('veuillez saisir votre email ')
-        mdp=input('veuillez saisir votre mot de pass ')
-        u=afficher_utilisateur(email,mdp)
-        if u != None:
-            main()
+        while True:
+            email=input('veuillez saisir votre email ')
+            if "@" in email and "." in email and " " not in email:
+                break
+            else:
+                print("Le mail saisi n'est pas valide")
+        hash_mot_de_passe=mot_de_pass_hash(email)
+        if hash_mot_de_passe == None:
+            print("cette email n'existe pas")
         else:
-            print('erreur de connexion')
-auth()
+            while True:
+                mdp=input('veuillez saisir votre mot de pass ')
+                if len(mdp) < 8:
+                    print('Le mot de pass doit contenir au moins 8 caracteres')
+                else:
+                    break
+            if bcrypt.checkpw(mdp.encode('utf-8'), hash_mot_de_passe.encode('utf-8')):
+                u=utilisateur_connecter(email,hash_mot_de_passe)
+                if u != None and u[0]=='user':
+                    print(f'Bienvenue {u[2]} {u[1]}')
+                    User()
+                elif u != None and u[0]=='admin':
+                    print(f'Bienvenue {u[2]} {u[1]}')
+                    Admin()    
+                else:
+                    print('erreur de connexion')
+            else:
+                print("Mot de passe incorrect")
+            
+            
+main()
 
--- model/utilisateur.py
+++ model/utilisateur.py
@@ -1,10 +1,11 @@
 from db import get_connection
-def creer_utilisateur(nom,prenom,telephone,email,mot_de_pass):
+import bcrypt
+def creer_utilisateur(nom,prenom,telephone,email,mot_de_pass,role):
     conn=get_connection()
     curseur=conn.cursor()
     try:
-        sql='insert into utilisateurs(nom,prenom,telephone,email,mot_de_pass)values(%s,%s,%s,%s,%s)'
-        curseur.execute(sql,(nom,prenom,telephone,email,mot_de_pass))
+        sql='insert into utilisateurs(nom,prenom,telephone,email,mot_de_pass,role)values(%s,%s,%s,%s,%s,%s)'
+        curseur.execute(sql,(nom,prenom,telephone,email,mot_de_pass,role))
         conn.commit()
         print('utilisateur ajouter avec succes')
     except Exception as e:
@@ -28,24 +29,52 @@ def Ajouter_utilisateur():
             break
     while True:
         telephone=input('saisir le numero de telephone')
-        if not telephone.isnumeric() and len(telephone)!=9:
+        if (not telephone.isnumeric()  or len(telephone) != 9 or telephone[:2] not in ["77", "78", "70", "71", "76","75"]): 
             print('saisi invalide')
         else:
             break
-    email=input("saisir l'email")
+    while True:
+            email=input("saisir l'email")
+            if "@" in email and "." in email and " " not in email:
+                break
+            else:
+                print("Le mail saisi n'est pas valide")
+    while True:
+        print('1: USER')
+        print('2: ADMIN')
+        choix=input('choisir le role')
+        if choix.isnumeric():
+            choix=int(choix)
+            match choix:
+                case 1:
+                    role='user'
+                    break
+                case 2:
+                    role='admin'
+                    break
+                case _:
+                    print('Choix invalide')
+        else:
+            print('vous devez saisir un nombre')
+        
     while True:
         mdp=input('saisir le mot de pass')
         if len(mdp)<8:
             print('le mot de pass doit contenir 8 caracteres')
         else:
-            break
-    creer_utilisateur(nom,prenom,telephone,email,mdp)
+            # convertir en bytes
+            mot_de_passe_bytes = mdp.encode('utf-8')
 
-def afficher_utilisateur(email,mdp):
+            # hachage
+            hash = bcrypt.hashpw(mot_de_passe_bytes, bcrypt.gensalt())
+            break
+        
+    creer_utilisateur(nom,prenom,telephone,email,hash,role)
+def utilisateur_connecter(email,mdp):
     conn=get_connection()
     curseur=conn.cursor()
     try:
-        sql='select * from utilisateurs where email=%s and mot_de_pass=%s'
+        sql='select role,prenom,nom from utilisateurs where email=%s and mot_de_pass=%s'
         curseur.execute(sql,(email,mdp))
         u=curseur.fetchone()
         return u
@@ -55,20 +84,37 @@ def afficher_utilisateur(email,mdp):
         curseur.close()
         conn.close()
 
-def afficherutilisateur():
+def afficher_utilisateur():
     conn=get_connection()
     curseur=conn.cursor()
     try:
-        sql='select * from utilisateurs'
+        sql='select nom,prenom,email,telephone from utilisateurs'
         curseur.execute(sql)
         utilisateurs=curseur.fetchall()
-        return utilisateurs
+        for u in utilisateurs:
+            print(f"Prenom: {u[0]} | Nom: {u[1]} | Email: {u[2]} | Telephone: {u[3]}")
     except Exception as e:
         print("erreur lors de l'affichage ",e)
     finally:
         curseur.close()
         conn.close()
 
+def mot_de_pass_hash(email):
+    conn=get_connection()
+    curseur=conn.cursor()
+    try:
+        sql='select mot_de_pass from utilisateurs where email=%s'
+        curseur.execute(sql,(email,))
+        mdp=curseur.fetchone()
+        if mdp==None:
+            return mdp
+        else:
+            return mdp[0]
+    except Exception as e:
+        print("erreur lors de l'affichage ",e)
+    finally:
+        curseur.close()
+        conn.close()
     
     
     
